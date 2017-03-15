a7974fc3a38672d72f95721bfc226bc7f4d15ebb
Morgan Lartigue <mlartigue@intechinfo.fr>
2017-03-15 17:27:00 +0100

    implemented interface in MyList and some unit tests

--- FirstSolution/IntechCode/IntechCollection/MyList.cs
+++ FirstSolution/IntechCode/IntechCollection/MyList.cs
@@ -4,32 +4,79 @@ namespace IntechCode.IntechCollection
 {
     public class MyList<T> : IMyList<T>
     {
+        private T[] _items;
+
+        private int _count;
+        
+        public MyList()
+        {
+            _items = new T[16];
+        }
         public T this[int index] 
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get
+            {
+                if(index >= _count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return _items[index];
+            }
+            set
+            {
+                if (index >= _count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                _items[index] = value;
+            }
         }
 
-        public int Count => throw new NotImplementedException();
-
-        public void Add(T item)
+        public int Count
         {
-            throw new NotImplementedException();
+            get
+            {
+                return _count;
+            }
         }
+
+        public void Add(T item) => Insert(_count, item);
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < _count; ++i) {
+                if(System.Collections.Generic.EqualityComparer<T>.Default.Equals(item, _items[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index > _count) throw new IndexOutOfRangeException();
+            if (_count == _items.Length)
+            {
+                // Realloc needed.
+                T[] newItems = new T[_items.Length * 2];
+                Array.Copy(_items, 0, newItems, 0, _count);
+                _items = newItems;
+            }
+            if (index == _count) _items[_count] = item;
+            else
+            {
+                Array.Copy(_items, index, _items, index + 1, _count - index);
+                _items[index] = item;
+            }
+            _count++;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= _count) throw new IndexOutOfRangeException();
+            Array.Copy(_items, index + 1, _items, index, _items.Length - (index + 1));
+            _items[--_count] = default(T);
         }
     }
 }
---
+++ FirstSolution/Tests/IntechCode.Tests/MyListTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using IntechCode.IntechCollection;
+using System;
+using Xunit;
+
+namespace IntechCode.Tests
+{
+    [NUnit.Framework.TestFixture]
+    public class MyListTests
+    {
+        [NUnit.Framework.Test]
+        [Fact]
+        public void MyList_is_initially_empty()
+        {
+            //Arrange
+            MyList<int> l = new MyList<int>();
+            //Act -- nothing
+            //Assert
+            l.Count.Should().Be(0);
+        }
+
+        [NUnit.Framework.Test]
+        [Fact]
+        public void Add_element_to_MyList_should_return_count_1()
+        {
+            //Arrange
+            MyList<int> l = new MyList<int>();
+            //Act -- nothing
+            l.Add(1);
+            //Assert
+            l.Count.Should().Be(1);
+        }
+
+        [NUnit.Framework.Test]
+        [Fact]
+        public void Add_element_to_MyList_when_table_is_full_should_return_l_16_equal_16()
+        {
+            //Arrange
+            MyList<int> l = new MyList<int>();
+            //Act -- nothing
+            for(int i = 0; i <= 16; i++)
+            {
+                l.Add(i);
+            }      
+            //Assert
+            l[16].Should().Be(16);
+        }
+
+        [NUnit.Framework.Test]
+        [Fact]
+        public void Remove_element_from_MyList_table_1_should_be_2()
+        {
+            //Arrange
+            MyList<int> l = new MyList<int>();
+            //Act -- nothing
+            for (int i = 0; i <= 2; i++)
+            {
+                l.Add(i);
+            }
+            l.RemoveAt(1);
+            //Assert
+            l[1].Should().Be(2);
+        }
+    }
+}
